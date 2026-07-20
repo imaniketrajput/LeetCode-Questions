c36105770df504e9d68fd5842ab4ec3b4247b61c
Aniket Singh <as.thakuraniket@gmail.com>
2026-07-21 01:13:21 +0530

    Time: 52 ms (81.66%), Space: 23.9 MB (57%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,55 +8,25 @@
  */
 class Solution {
 public:
-
-    int getLength(ListNode* head)
-    {
-        ListNode* temp = head;
-        int len = 0;
-        while(temp)
-        {
-            len++;
-            temp = temp->next;
-        }
-
-        return len;
-    }
-
-    ListNode* collisionPoint(ListNode *headA, ListNode *headB, int d)
-    {
-        ListNode* t1 = headA;
-        ListNode* t2 = headB;
-
-        while(d){
-            d--;
-            t2 = t2->next;
-        }
-
-        while(t1 != t2){
-            t1 = t1->next;
-            t2 = t2->next;
-        }
-
-        return t1;
-    }
-
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-        int N1 = getLength(headA);
-        int N2 = getLength(headB);
-
-        if(N2 > N1)
+        if(headA == nullptr || headB == nullptr) return nullptr;
+        
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+        
+        while(t1 != t2)
         {
-            return collisionPoint(headA, headB, N2-N1);
+            t1 = t1->next;
+            t2 = t2->next;
+            
+            if(t1 == t2) return t1;
+            
+            if(t1 == nullptr) t1 = headB;
+            
+            if(t2 == nullptr) t2 = headA;
         }
-        else{
-            return collisionPoint(headB, headA, N1-N2);
-
-        }
-
-        return nullptr;
-
-
-           
+        
+        return t1;
     }
 };
