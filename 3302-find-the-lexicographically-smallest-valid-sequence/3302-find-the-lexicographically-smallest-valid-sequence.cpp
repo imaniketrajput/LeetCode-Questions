e6a151e4556e37ff9ca0a6addee7a56a2b3787b1
Aniket Singh <as.thakuraniket@gmail.com>
2026-08-08 19:46:18 +0530

    Time: 63 ms (7.45%), Space: 121 MB (15.96%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        vector<int> rightMatch(n, 0);
+
+        int rightMatchCount = 0;
+        int i = n-1;
+        int j = m-1;
+
+        while(i >= 0){
+            if(j >=0 && word1[i]==word2[j])
+            {
+                rightMatchCount++;
+                j--;
+            }
+
+            rightMatch[i] = rightMatchCount;
+            i--;
+
+        }
+
+        vector<int> seq;
+
+        bool changePower = true;
+
+        i=0;
+        j=0;
+
+        while(i<n && j<m)
+        {
+            if(word1[i] == word2[j])
+            {
+                seq.push_back(i);
+                j++;
+            }
+            else if(changePower == true && i+1 < n && rightMatch[i+1] >= m-j-1)
+            {
+                seq.push_back(i);
+                j++;
+                changePower = false;
+            }
+
+            i++;
+        }
+
+        return j==m ? seq : vector<int>();
+    }
+};
