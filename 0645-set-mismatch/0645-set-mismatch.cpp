75eb0c41411441d604227088db85c5601bee27e1
Aniket Singh <as.thakuraniket@gmail.com>
2026-08-27 22:48:43 +0530

    Time: 0 ms (100%), Space: 25.1 MB (73.94%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+
+        int dup = -1;
+        int missing = -1;
+
+        for(int i=0; i<n; i++)
+        {
+            int idx = abs(nums[i]) - 1;
+
+            if(nums[idx] < 0){
+                dup = abs(nums[i]);
+            }
+            else{
+                
+                nums[idx] *= (-1);
+            }
+
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] > 0)
+            {
+                missing = i+1;
+                break;
+            }
+        }
+
+
+        return {dup, missing};
+    }
+};
