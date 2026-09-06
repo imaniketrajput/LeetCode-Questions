c9d9a031fa91bd0f97794f26d431359820de50cc
Aniket Singh <as.thakuraniket@gmail.com>
2026-09-07 01:11:51 +0530

    Time: 15 ms (82.63%), Space: 9.1 MB (86.25%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -6,17 +6,11 @@ public:
         int m = s.length();
         int n = t.length();
 
-        vector< vector<ull>> dp(m+1, vector<ull>(n+1));
+        vector<ull> curr(n+1, 0);
+        vector<ull> prev(n+1, 0);
 
-        for(int i=0; i<=m; i++)
-        {
-            dp[i][0] = 1;
-        }
-
-        for(int i=1; i<=n; i++)
-        {
-            dp[0][i] = 0;
-        }
+        prev[0] = 1;
+        curr[0] = 1;
 
         for(int i=1; i<=m; i++)
         {
@@ -24,14 +18,15 @@ public:
             {
                 if(s[i-1] == t[j-1])
                 {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 }
                 else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
